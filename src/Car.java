0e05fbe3ae740c97125fa818846b398f65492897
Рустам <rustam19871203@gmail.com>
2022-11-07 14:41:55 +0400

    ООп. Домашка 1, задача 3

--- src/Car.java
+++ src/Car.java
@@ -5,21 +5,8 @@ public class Car {
     public String color;
     public int year;
     public String country;
-
-    public Car(String brand, String model, int year,String country, String color, double engineVolume){
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.color = color;
-        this.country = country;
-        this.engineVolume = engineVolume;
-    }
     void info() {
-        System.out.println(brand + " " + model + ", " + year + " год выпуска, " + "сборка " + country + ", цвет " + color + ", объем двигателя- " + engineVolume + "л.");
+        System.out.println(brand + " " + model + ", " + year + " год выпуска, " + "сборка " + country + ", цвет " + color + ", объем двигателя-" + engineVolume + "л.");
         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-    }
-
-    public String toString(){
-        return brand + " " + model + ", " + year + " год выпуска, " + " сборка " + country + ", цвет " + color + " объем двигателя- " + engineVolume + "л.";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Car ladaGranta = new Car("LADA", "Granta", 2015, "Россия", "желтый", 1.7);
-        Car audiA8 = new Car("AUDI", "A8 50L TDI quattro", 2020, "Германия", "черный", 3.0);
-        Car bmwZ8 = new Car("BMW", "Z8", 2021, "Германия", "черный", 3.0);
-        Car kiaSportage = new Car("KIA", "Sportage 4-го поколения", 2018, "Южная Корея", "красный", 2.4);
-        Car hyundaiAvante = new Car("HYUNDAI", "Avante", 2016, "Южная корея", "оранжевый", 1.6);
+
+        Car ladaGranta = new Car();
+        ladaGranta.brand = "LADA"; ladaGranta.model = "Granta"; ladaGranta.year = 2015; ladaGranta.color = "желтый"; ladaGranta.country = "Россия"; ladaGranta.engineVolume = 1.7;
+        Car audiA8 = new Car();
+        audiA8.brand = "AUDI"; audiA8.model = "A8 50L TDI quattro"; audiA8.year = 2020; audiA8.color = "черный"; audiA8.country = "Германия"; audiA8.engineVolume = 3.0;
+        Car bmwZ8 = new Car();
+        bmwZ8.brand = "BMW"; bmwZ8.model = "Z8"; bmwZ8.year = 2021; bmwZ8.color = "черный"; bmwZ8.country = "Германия"; bmwZ8.engineVolume = 3.0;
+        Car kiaSportage = new Car();
+        kiaSportage.brand = "KIA"; kiaSportage.model = "Sportage"; kiaSportage.year = 2018; kiaSportage.color = "красный"; kiaSportage.country = "Южная Корея"; kiaSportage.engineVolume = 2.4;
+        Car hyundaiAvante = new Car();
+        hyundaiAvante.brand = "HYUNDAI"; hyundaiAvante.model = "Avante"; hyundaiAvante.year = 2016; hyundaiAvante.color = "оранжевый"; hyundaiAvante.country = "Южная Корея"; hyundaiAvante.engineVolume = 1.6;
 
         ladaGranta.info();
         audiA8.info();
         bmwZ8.info();
         kiaSportage.info();
         hyundaiAvante.info();
-
-        System.out.println(ladaGranta.toString());
-        System.out.println(audiA8.toString());
-        System.out.println(bmwZ8.toString());
-        System.out.println(kiaSportage.toString());
-        System.out.println(hyundaiAvante.toString());
     }
 }
