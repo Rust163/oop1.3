044629ff57957bf1be42c0b70aee44dec4562be4
Рустам <rustam19871203@gmail.com>
2022-11-08 11:38:54 +0400

    ООп. Домашка 3, задача 2

--- src/Car.java
+++ src/Car.java
@@ -5,8 +5,47 @@ public class Car {
     public String color;
     public int year;
     public String country;
+
+    public Car(String brand, String model, int year,String country, String color, double engineVolume){
+        if(brand.length() == 0) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+
+        if(model.length() == 0) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+
+        this.year = year;
+
+        if (color.length() == 0) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+
+        if(country.length() == 0) {
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
+
+        if (engineVolume >= 0){
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+    }
     void info() {
-        System.out.println(brand + " " + model + ", " + year + " год выпуска, " + "сборка " + country + ", цвет " + color + ", объем двигателя-" + engineVolume + "л.");
         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(brand + " " + model + ", " + year + " год выпуска, " + "сборка " + country + ", цвет " + color + ", объем двигателя- " + engineVolume + "л.");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+    }
+    //--------------С использование метода toString______________________________
+    public String toString(){
+        return brand + " " + model + ", " + year + " год выпуска, " + " сборка " + country + ", цвет " + color + ", объем двигателя- " + engineVolume + "л.";
     }
 }
